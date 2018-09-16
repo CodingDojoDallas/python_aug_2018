747232e1e0d6f979cfa195d8810334377a777b86
Riya Wasnik <riya@riyas-air.attlocal.net>
2018-09-16 17:02:55 -0500

    deletion

--- riya_wasnik/city/apps/city_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CityAppConfig(AppConfig):
-    name = 'city_app'
--- riya_wasnik/login/main/apps/login_app/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.conf.urls import url, include
-from . import views
-urlpatterns = [
-    url(r'^$', views.home ),
-    url(r'^create$', views.process),
-    url(r'^success$', views.success),
-    url(r'^login$', views.login)
-]
